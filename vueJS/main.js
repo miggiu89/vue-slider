9be64dc4272f3002acc1e38395cc6bc8ae45f21f
Matteo <matteomigliori280489@gmail.com>
2021-03-20 13:08:04 +0100

    slider next ok

---
+++ vueJS/main.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+
+    el: '#root',
+
+    data: {
+
+        indexImg : 0 ,
+        images: ['img/img0.jpg','img/img1.jpg','img/img2.jpg','img/img3.jpg']
+    },
+
+    methods:{
+
+        next : function(){
+
+            if(this.indexImg == this.images.length -1){
+                this.indexImg = 0;
+            }
+            else{
+                this.indexImg++;
+            }
+        },
+
+        prev : function(){
+
+            
+        }
+    }
+
+})
