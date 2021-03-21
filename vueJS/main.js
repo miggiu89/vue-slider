ab1de5ad2e75682638d4cf66a4c32e5517c3ebf6
Matteo <matteomigliori280489@gmail.com>
2021-03-21 22:38:15 +0100

    push

--- vueJS/main.js
+++ vueJS/main.js
@@ -4,8 +4,17 @@ var app = new Vue({
 
     data: {
 
+        circleActive : 0,
         indexImg : 0 ,
         images: ['img/img0.jpg','img/img1.jpg','img/img2.jpg','img/img3.jpg']
+    },
+
+    created: function(){
+        setInterval(() =>{
+            this.next()
+        }, 2000);
+
+
     },
 
     methods:{
@@ -13,16 +22,16 @@ var app = new Vue({
         next : function(){
 
             (this.indexImg == this.images.length -1) ? this.indexImg = 0 : this.indexImg++;
-                
+            (this.circleActive == this.images.length -1) ? this.circleActive = 0 : this.circleActive++;
            
         },
 
         prev : function(){
 
             (this.indexImg == 0) ? this.indexImg = this.images.length = -1 : this.indexImg--;
+            (this.circleActive == 0) ? this.circleActive = this.circleActive = -1 : this.indexImg--;
         },
 
-        
         
         
     }
